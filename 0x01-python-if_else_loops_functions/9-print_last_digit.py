023b8f9843c64bd53afd2b21d4c9c232aabdbf04
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-08 13:36:58 -0700

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,7 @@ def print_last_digit(number):
     if number < 0:
         num = number * -1
         num = num % 10
-        num = num * -1
     else:
         num = number % 10
+    print(num, end='')
     return num
