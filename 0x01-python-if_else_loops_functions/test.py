a07dba88b2c00df66e80d1744d361da1031ccab3
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-08 15:15:55 -0700

    fizzzBuzzz

---
+++ 0x01-python-if_else_loops_functions/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for num in range(1, 101):
+        if num % 3 == 0:
+            print('{}'.format('Fizz'), end=' ')
+        elif num % 5 == 0:
+            print('{}'.format('Buzz'), end=' ')
+        elif num % 3 == 0 and num % 5 == 0:
+            print('{}'.format('FizzBuzz'), end=' ')
+        else:
+            print('{}'.format(num), end=' ')
