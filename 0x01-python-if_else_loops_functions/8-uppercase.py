45f8a10b29bd3fbb05aa36cb739c66f50d86e97e
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-08 11:02:10 -0700

    is upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     if ord(str) >= 97 and ord(str) <= 122:
-        ord(str) = ord(str) - 32
+        ord(str2) = ord(str) - 32
         print('{str2}')
     else:
         print('{str}')
