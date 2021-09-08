40f8d1e415dc6ef5b52c7e8d4160fd20745f54f9
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-08 07:15:53 -0700

    last digit pressure

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ if number < 0:
     num = num * -1
 else:
     num = number % 10
-print('Last digit of {} is {} and is'.format(number, num),end='')
+print('Last digit of {} is {} and is'.format(number, num), end=' ')
 if num > 5:
     print('greater than 5')
 elif num == 0:
