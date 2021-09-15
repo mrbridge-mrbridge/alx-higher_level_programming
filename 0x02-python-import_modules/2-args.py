e766b8ecab03e1ba4be8fbe49030da5247844552
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-15 07:55:39 -0700

    argument lists

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    i = 1
+    if len(argv) = 1:
+        print('{} argument:'.format(len(argv)))
+    elif len(argv) = 0:
+        print('{} arguments.'.format(len(argv)))
+    else:
+        print('{} arguments:'.format(len(argv)))
+
+    if len(argv) >= 1:
+        for i in range(1, len(argv)+1), 1:
+            print('{}: {}'.format(i, argv[i]))
+
