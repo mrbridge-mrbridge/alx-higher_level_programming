a464c5d8aab9233262099502b90bb89d7f82e2f3
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-14 20:56:25 -0700

    import multiple functions

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__"    
+if __name__ == "__main__":    
     from calculator_1 import add
     from calculator_1 import sub
     from calculator_1 import mul
