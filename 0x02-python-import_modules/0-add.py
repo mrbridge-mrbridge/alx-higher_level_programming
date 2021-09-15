8a032343d3c87a015e87ca8cb51071f0a5522226
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-14 20:30:46 -0700

    __name__ edit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,5 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    p = add(a, b)
-    print('{} + {} = {}'.format(a, b, p))
+    print('{} + {} = {}'.format(a, b, add(a, b)))
 
