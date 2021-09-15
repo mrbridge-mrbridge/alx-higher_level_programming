2c24ba002fa86834e2affc6526b37e6017bc9816
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-15 08:05:45 -0700

    argv count

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""if __name__ == '__main__':"""
-    import sys
+if __name__ == '__main__':
+   from sys import argv
     if len(argv) = 1:
         print('{} argument:'.format(len(argv)))
     elif len(argv) = 0:
