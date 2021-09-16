27b991515c663070431793a52708d4c27a88effa
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-16 14:24:14 -0700

    indent

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   from sys import argv
-   count = len(argv) - 1
-   if count == 1:
-       print("1 argument:")
-   elif count == 0:
-       print("0 arguments.")
-   else:
-       print("{} arguments:".format(count))
-   for i in range(1, count+1, 1):
+    from sys import argv
+    count = len(argv) - 1
+    if count == 1:
+        print("1 argument:")
+    elif count == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(1, count+1, 1):
         print("{}: {}".format(i, argv[i]))
