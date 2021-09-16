f0dbf4af63a374c4b177e082d25a4c87fc71eae7
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-16 14:55:49 -0700

    ch

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,18 +5,19 @@ if __name__ == "__main__":
     count = len(argv) - 1
     a = int(argv[1])
     b = int(argv[3])
+    op = ['+', '-', '*', '/']
     if count != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        exit(1)
+        sys.exit(1)
+
+    if argv[2] == '+':
+        print('{} {} {} = {}'.format(a, argv[2], b, add(a, b))
+    elif argv[2] == '-':
+        print('{} {} {} = {}'.format(a, argv[2], b, sub(a, b))
+    elif argv[2] == '*':
+        print('{} {} {} = {}'.format(a, argv[2], b, mul(a, b))
+    elif argv[2] == '/':
+        print('{} {} {} = {}'.format(a, argv[2], b, div(a, b))
     else:
-        if int(argv[2]) == '+':
-            print('{} {} {} = {}'.format(a, argv[2], b, add(a, b))
-        elif int(argv[2]) == '-':
-            print('{} {} {} = {}'.format(a, argv[2], b, sub(a, b))
-        elif int(argv[2]) == '*':
-            print('{} {} {} = {}'.format(a, argv[2], b, mul(a, b))
-        elif int(argv[2]) == '/':
-            print('{} {} {} = {}'.format(a, argv[2], b, div(a, b))
-        else:
-            print('Unknown operator. Available operators: +, -, * and /')
-            exit(1)
+        print('Unknown operator. Available operators: +, -, * and /')
+        sys.exit(1)
