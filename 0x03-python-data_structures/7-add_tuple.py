a190fe7e8b83ccdd3783a1249d36d18cae8b73f5
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-19 18:05:58 -0700

    tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tp = ()
+    tp, tuple_y, tuple_x = (), (), ()
     for i in range(2):
         if tuple_a[i] == ():
-            tuple_a[i] = 0
+            tuple_x[i] = 0
+        else:
+            tuple_x[i] = tuple_a[i]
+
         if tuple_b[i] == ():
-            tuple_b[i] = 0
-        tp[i] = tuple_a[i] + tuple_b[i]
+            tuple_y[i] = 0
+        else:
+            tuple_y[i] = tuple_a[i]
+        tp[i] = tuple_x[i] + tuple_y[i]
     return tp
