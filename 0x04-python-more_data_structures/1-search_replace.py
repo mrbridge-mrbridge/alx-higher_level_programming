3a02d5504d17b999a550e395a3116072525553d6
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-21 06:25:31 -0700

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = my_list[:]
-    for i in range(len(new) - 1):
+    for i in range(len(new)):
         if new[i] == search:
             new[i] = replace
     return new
