c97d62f990753a7d63b82ccef84171f1d65f450c
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-21 05:36:19 -0700

    insert or replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     new = my_list[:]
     for i in range(len(new) - 1):
         if new[i] == search:
-            new[i] = search
+            new.insert(i, replace)
     return new
