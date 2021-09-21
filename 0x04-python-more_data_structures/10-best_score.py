6a12e4375bfa88b08f5d0f604498747d7cfb82c1
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-21 12:43:51 -0700

    key with big value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    big = list(a_dictionary.keys())[0]
-    for k in a_dictionary:
-        if a_dictionary[k] > big:
-            big = a_dictionary[k]
-            ret = k
+    big = 0
     if len(a_dictionary) == 0:
         return None
     else:
-        return (ret)
+        for k in a_dictionary:
+            if a_dictionary[k] > big:
+                big = a_dictionary[k]
+                ret = k
+            return (ret)
