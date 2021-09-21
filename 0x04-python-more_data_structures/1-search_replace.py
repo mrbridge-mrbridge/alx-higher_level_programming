1b66daa842384f9194a6f133ec56588d643a4b47
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-21 05:03:19 -0700

    replace an elm

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list
-    for elm in new_list:
-        if elm == search:
-            elm = replace
+    for i in range(len(new_list) - 1):
+        for elm[i] in new_list:
+            if elm[i] == search:
+                elm[i] = replace
     return new_list
 
