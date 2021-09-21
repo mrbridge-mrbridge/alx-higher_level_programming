0a028b77a27b2aa2ae339447922edc2127d2dca0
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-21 08:08:34 -0700

    add unique all

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-__import__('functools').reduce
 def uniq_add(my_list=[]):
+    total = 0
     n = set(my_list[:])
-    return (reduce(lambda x, y: x + y, n))
-
+    for elm in n:
+        total += elm
+    return total
