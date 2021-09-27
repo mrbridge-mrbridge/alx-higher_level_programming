b71bdf21b5b63d3e0ff08edb1bf644de023a3cb8
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-27 16:28:13 -0700

    real int

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = o
+    count = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
