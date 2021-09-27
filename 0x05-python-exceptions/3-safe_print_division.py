e7cf4bf67eb437bbbf3ccdd79c8c54c87a593047
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-27 16:40:46 -0700

    zerodivisionerror

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         res = a / b
     except ZeroDivisionError:
-        res None
+        res = None
     finally:
         print('Inside result:{}'.format(res))
     return res
