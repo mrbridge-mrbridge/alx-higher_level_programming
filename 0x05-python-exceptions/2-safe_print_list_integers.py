d5cbe3ad48a87390f8cc54a3422b3b6aa9578738
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-27 16:25:54 -0700

    print real int only

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     count = o
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i] if int(my_list[i]) else continue), end="")
+            print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (IndexError, TypeError):
-            break
+        except (ValueError, TypeError):
+            continue
+    print("")
     return count
