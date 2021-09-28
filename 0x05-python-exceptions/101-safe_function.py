5f7a88223988fafb268c156b82b8925511ac3523
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-27 18:05:48 -0700

    function executes safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
 def safe_function(fct, *args):
     try:
         res = fct(*args)
         return res
     except Exception as ex:
+        import sys
         print('Exception: {}'.format(ex), file=sys.stderr)
         return None
