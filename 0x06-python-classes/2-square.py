d6be9e0200dd7eeb9204e7593122e9d1d138a5a1
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-28 19:01:49 -0700

    raise exceptions with priv inst att

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,14 +10,8 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        try:
-            self.__size = size
-            if type(size) is not int:
-                raise TypeError
-            elif size < 0:
-                raise ValueError
-        
-        except TypeError:
-            print('size must be an integer')
-        except ValueError:
-            print('size must be >= 0')
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
