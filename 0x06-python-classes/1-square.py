b8f8a41f86986e3bbf0e60c1102b9d6046356abc
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-09-28 18:31:42 -0700

    private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A class Square"""
+class Square:
+    """Representing a Square"""
+
+    def __init__(self, size):
+        """Initialize a method Square
+
+        Args:
+            size(int): The size of a side of the Square
+        """
+        self.__size = size
