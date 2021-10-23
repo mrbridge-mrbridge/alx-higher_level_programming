c28a2dcae119556fa0937a2ce5c62f25eeb86b61
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-10-23 15:11:52 -0700

    rectangle dict

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -113,3 +113,13 @@ class Rectangle(Base):
                 self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """returns dictionary representation of class"""
+        return {
+                "id": self.id,
+                "width": self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
+                }
