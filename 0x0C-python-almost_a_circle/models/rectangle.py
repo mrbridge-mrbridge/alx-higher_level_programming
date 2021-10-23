f3b771d06c730e06e17826b87cd592ed1c5782ba
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-10-22 18:50:19 -0700

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,4 +83,7 @@ class Rectangle(Base):
     def __str__(self):
         """returns rectangle details"""
         return ("[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id, 
-                self.__x, self.__y, self.__width, self.__height))
+                self.__x, 
+                self.__y, 
+                self.__width, 
+                self.__height))
