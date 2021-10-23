743f71feade7d87ca2ca3ecfd93bdcc7d8d6d799
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-10-22 17:04:55 -0700

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module contains the Base class"""
+
+class Base:
+    """The class Base """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialises the class base"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
