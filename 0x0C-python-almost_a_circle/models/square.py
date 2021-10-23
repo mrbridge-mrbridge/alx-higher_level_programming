292041028527093e940fba1aa81046fc0acf7827
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-10-23 15:23:29 -0700

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -50,3 +50,12 @@ class Square(Rectangle):
                 self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """dict representation of square"""
+        return {
+                "id" = self.id,
+                "size" = self.size,
+                "x" = self.x,
+                "y" = self.y
+                }
