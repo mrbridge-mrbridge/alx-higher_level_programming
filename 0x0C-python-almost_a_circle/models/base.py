1e905bc09e6ddf0c28b94331fca2cafbc3d6ae63
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-10-23 15:38:17 -0700

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module contains the Base class"""
+import json
 
 
 class Base:
@@ -13,3 +14,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns json string representation of 
+        list_dictionaries
+        """
+        if len(list_dictionaries) = 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
