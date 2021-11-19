b895079f2bb8ff049fa1f841062dc37599d47ba9
root <nanamcroj@gmail.com>
2021-11-18 21:02:06 -0800

    table

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,4 +3,4 @@
 -- Database hbtn_0c_0
 -- Table first_table
 -- Field name in first_table
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci; USE hbtn_0c_0; ALTER TABLE first_table CONVERT TO CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; USE hbtn_0c_0; ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
