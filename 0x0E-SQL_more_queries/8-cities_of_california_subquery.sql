e0beac93762561901781b12ce69b0b5c48eee264
root <nanamcroj@gmail.com>
2021-11-21 07:49:59 -0800

    select from two

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+-- The states table contains only one record where name = California (but the id can be different)
+-- Results must be sorted in ascending order by cities.id
+-- You are not allowed to use the JOIN keyword
+SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id=(SELECT id FROM states WHERE name='California') ORDER BY id ASC;
