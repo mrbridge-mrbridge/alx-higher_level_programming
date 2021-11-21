06c0717a2aefde0fe21e91643c63679b8ae3f877
root <nanamcroj@gmail.com>
2021-11-21 07:53:17 -0800

    select from two

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,4 @@
 -- The states table contains only one record where name = California (but the id can be different)
 -- Results must be sorted in ascending order by cities.id
 -- You are not allowed to use the JOIN keyword
-SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id=(SELECT id FROM states WHERE name='California') ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id=(SELECT id FROM states WHERE name='California') ORDER BY id;
