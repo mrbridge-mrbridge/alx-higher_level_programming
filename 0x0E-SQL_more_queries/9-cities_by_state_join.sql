d5bf129c8506a32c057de850515a3167d2d4106b
root <nanamcroj@gmail.com>
2021-11-21 08:12:50 -0800

    states join cities

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- lists all cities contained in the database hbtn_0d_usa
+-- Results must be sorted in ascending order by cities.id
+-- You can use only one SELECT statement
+-- Each record should display: cities.id - cities.name - states.name
+SELECT cities.id, cities.name, states.name 
+FROM cities INNER JOIN states 
+ON cities.state_id=states.id 
+ORDER BY cities.id;
