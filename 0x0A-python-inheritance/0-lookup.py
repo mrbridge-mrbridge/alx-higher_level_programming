3e1af73888f8520ea398c75c5011ba20ae68c1da
root <nanamcroj@gmail.com>
2021-12-06 05:11:11 -0800

    dir(obj)

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module returns the list of available attributes and methods of an object"""
+
+def lookup(obj):
+    """function accepts obj as input and list all 
+        available attributes and methods of an obj
+    """
+    return dir(obj)
