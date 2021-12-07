73f9def0efa41cbdee3892fbbdd60b4a6f757872
root <nanamcroj@gmail.com>
2021-12-06 18:04:09 -0800

    subclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module returns True if the object is an instance of a class
+that inherited (directly or indirectly) from the
+specified class ; otherwise False"""
+
+
+def inherits_from(obj, a_class):
+    """returns true if obj is subclass of a_class. Else false"""
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
