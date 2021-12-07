022fa2800d7d3c847b12d3a2ce4a666e0830be9f
root <nanamcroj@gmail.com>
2021-12-06 16:29:16 -0800

    inherit list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module that inherits from list 
+prints the list, but sorted (ascending sort)
+"""
+
+
+class MyList(list):
+    """class 'MyList' inherits from class 'list'"""
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
