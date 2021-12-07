811a0379af18dd587a9dc62b24dd4d412c16a8f5
root <nanamcroj@gmail.com>
2021-12-07 06:50:36 -0800

    upate

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,6 +5,7 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """import parent class"""
 
+
 class Rectangle(BaseGeometry):
     """declare class Rectangle as inheriting from class BaseGeometry"""
     def __init__(self, width, height):
