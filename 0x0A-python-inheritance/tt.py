d6d2047af8d00eb1c1dbcf4faa38abf94503e0b1
root <nanamcroj@gmail.com>
2021-12-07 06:44:07 -0800

    upate

--- 0x0A-python-inheritance/tt.py
+++ 0x0A-python-inheritance/tt.py
@@ -3,7 +3,19 @@
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""import parent class"""
+class BaseGeometry:
+    """BaseGeometry class declared"""
+    def area(self):
+        """raises an exception only"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
     """declare class Rectangle as inheriting from class BaseGeometry"""
