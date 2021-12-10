6e06baaa8d12c6ad17fbf8cc9df03030cdb98811
root <nanamcroj@gmail.com>
2021-12-10 11:54:46 -0800

    indent changed

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""
-    writes a string to a text file (UTF8) and
-    returns the number of characters written
-"""
+""" writes a string to a text file (UTF8) and
+    returns the number of characters written"""
 
 
 def write_file(filename="", text=""):
-"""
-    writes a string to a text file (UTF8) and
-    returns the number of characters written
-"""
-"""Writes a string to a text file (UTF8) and returns
+    """
+    Writes a string to a text file (UTF8) and returns
     the number of characters written
     Must use the with statement
     Should create the file if doesn’t exist
-"""
+    """
     with open(filename, 'w', encoding='utf8') as f:
         return f.write(text)
