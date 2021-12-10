b7ad0d82143a7976acd3457a28d67f6f7669ebcf
root <nanamcroj@gmail.com>
2021-12-10 09:53:44 -0800

    reversed eq and ne

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module with class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    """class overturns the use of == and != """
+    def __eq__(self, value):
+        """== becomes !="""
+        return int(self) != value
+        
+    def __ne__(self, value):
+        """!= becomes =="""
+        return int(self) == value
