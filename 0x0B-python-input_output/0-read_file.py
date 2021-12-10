72c0248b9a4f140ee8c8d295257fd1652d7bcc6d
root <nanamcroj@gmail.com>
2021-12-10 11:15:02 -0800

    read and print file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding='utf8') as f:
+        print(f.read(), end='')
