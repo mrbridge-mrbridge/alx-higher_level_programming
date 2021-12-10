0a8766d8348720bd2b2318fdc9c02575dfa035bf
root <nanamcroj@gmail.com>
2021-12-10 11:48:29 -0800

    change

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Writes a string to a text file (UTF8) and returns
-    the number of characters written
-    Must use the with statement
-    Should create the file if doesn’t exist
+"""
+    writes a string to a text file (UTF8) and
+    returns the number of characters written
 """
 
 
@@ -10,6 +9,11 @@ def write_file(filename="", text=""):
 """
     writes a string to a text file (UTF8) and
     returns the number of characters written
+"""
+"""Writes a string to a text file (UTF8) and returns
+    the number of characters written
+    Must use the with statement
+    Should create the file if doesn’t exist
 """
     with open(filename, 'w', encoding='utf8') as f:
         return f.write(text)
