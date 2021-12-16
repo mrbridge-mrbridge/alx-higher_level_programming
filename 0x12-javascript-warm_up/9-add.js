6b7e20b273458d6456ed7e390f8198301b1cd0dc
root <nanamcroj@gmail.com>
2021-12-16 03:20:43 -0800

    add funx

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 'use strict';
 const a = Number(process.argv[2]);
 const b = Number(process.argv[3]);
-function add(a, b) {
-  return (a + b)
+function add (a, b) {
+  return (a + b);
 }
 console.log(add(a, b));
