efa8a07a61ec7b8d483abc4510cf1f1c5b89ab80
root <nanamcroj@gmail.com>
2021-12-16 09:23:55 -0800

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,6 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   let args = process.argv.slice(2);
-/*  args = args.map(Number);*/
   args = args.sort((a, b) => a - b).reverse();
-  console.log(args[1])
+  console.log(args[1]);
 }
