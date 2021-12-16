c433977f6fa7edd670a543aeff0247edd8c6eb98
root <nanamcroj@gmail.com>
2021-12-16 12:10:05 -0800

    g

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+'use strict';
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
