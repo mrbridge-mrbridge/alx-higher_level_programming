aaaf6efc25ab358bcf99a0973b391c5db8c39d5c
root <nanamcroj@gmail.com>
2021-12-15 16:14:16 -0800

    argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 'use strict';
 let num = process.argv.length;
-if (num === 2){
-	console.log("No argument");
-} else if (num === 3){
-        console.log("Argument found");
+if (num === 2) {
+	console.log('No argument');
+} else if (num === 3) {
+        console.log('Argument found');
 } else {
-        console.log("Arguments found");
+        console.log('Arguments found');
 }
