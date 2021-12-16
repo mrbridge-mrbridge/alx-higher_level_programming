8a1f695cae4ff9bf5de3e6e1e41e662ba5863bc6
root <nanamcroj@gmail.com>
2021-12-16 00:49:16 -0800

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 'use strict';
 let num = process.argv.length;
 if (num === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (num === 3) {
-        console.log('Argument found');
+  console.log('Argument found');
 } else {
-        console.log('Arguments found');
+  console.log('Arguments found');
 }
