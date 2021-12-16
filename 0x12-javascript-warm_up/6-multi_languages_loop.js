70490125628899a2b9ccb6257ff51d7215ee9d59
root <nanamcroj@gmail.com>
2021-12-16 01:12:56 -0800

    semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 'use strict';
-let ar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const ar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i;
 for (i = 0; i < 3; i++) {
   console.log(ar[i]);
