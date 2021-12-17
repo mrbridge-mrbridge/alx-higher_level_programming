e2e1eb69dfb92e7fe429427e86458bd48a10a274
root <nanamcroj@gmail.com>
2021-12-16 17:45:26 -0800

    positive args

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
 'use strict';
-
+module.exports = class Rectangle {
+  if (w > 0 && h > 0) {
+      constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
