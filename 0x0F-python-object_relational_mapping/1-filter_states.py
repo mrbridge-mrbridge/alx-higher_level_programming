9e2cd92128b1e38540d6f0c7cb6fd542c051f187
root <nanamcroj@gmail.com>
2022-01-01 20:03:39 -0800

    capital N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that lists all states with a name starting with N from 
+script that lists all states with a name starting with N from
 the database hbtn_0e_0_usa
 """
 
