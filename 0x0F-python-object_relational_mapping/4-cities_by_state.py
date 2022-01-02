8be8bf5d7ce786a31e5639888aef1bc10f2e6ed3
root <nanamcroj@gmail.com>
2022-01-01 21:17:47 -0800

    cities list

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script that lists all cities from the database hbtn_0e_4_us
+a"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute('SELECT * FROM cities ORDER BY id ASC')
+    [print(state) for state in cur.fetchall()]
