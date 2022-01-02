b9bd27c83bf801a21c610fed0946fbba0b4c965e
root <nanamcroj@gmail.com>
2022-01-02 09:52:15 -0800

    update

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+lists all State objects that contain the letter a from the database
+hbtn_0e_6_usa
 You must use the module SQLAlchemy
 """
 
