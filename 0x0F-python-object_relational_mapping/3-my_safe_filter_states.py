379da1b1fbc10533130e9c82e19ec9752c6072c1
root <nanamcroj@gmail.com>
2022-01-01 21:04:32 -0800

    injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name = "{}"\
+                ORDER BY id ASC'.format(argv[4]))
+    [print(state) for state in cur.fetchall() if state[1] == argv[4]]
