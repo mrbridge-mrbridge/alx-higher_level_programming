7ef80535f3489271e14b259e071a92d656c8db63
root <nanamcroj@gmail.com>
2022-01-02 07:16:59 -0800

    py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that takes in the name of a state as an argument and lists all 
+script that takes in the name of a state as an argument and lists all
 cities of that state, using the database hbtn_0e_4_usa
 """
 import MySQLdb
