64831d669ed552ad2847b1fcb9419d0a4876467e
root <nanamcroj@gmail.com>
2022-01-01 20:47:41 -0800

    argv 4

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that takes in an argument and displays all values in the 
+script that takes in an argument and displays all values in the
 states table of hbtn_0e_0_usa where name matches the argument
 """
 
@@ -12,6 +12,5 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE ":s" \
-                ORDER BY id ASC'.format(argv[4]))
+    cur.execute('SELECT * FROM states WHERE name LIKE "{}" ORDER BY id ASC'.format(argv[4]))
     [print(state) for state in cur.fetchall()]
