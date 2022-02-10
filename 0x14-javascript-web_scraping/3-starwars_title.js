a56aab5ab874fd8fc4dad424f21d3a55e6cf6edf
root <nanamcroj@gmail.com>
2022-02-10 23:20:17 +0000

    swapi api

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi-api.hbtn.io/ap/films/' + process.argv[2];
+let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 function swapi (error, response, body) {
   if (error) {
     console.log(error);
