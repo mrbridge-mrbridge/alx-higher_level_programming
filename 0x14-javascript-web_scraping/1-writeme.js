e4102e584f1cf237c3aee96339611f158ded3dc0
root <nanamcroj@gmail.com>
2022-02-10 15:10:57 +0000

    writeFile

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.write(process.argv[2], process.argv[3],'utf8', (err, data) => {
+fs.writeFile(process.argv[2], process.argv[3],'utf8', err => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data);
 });
