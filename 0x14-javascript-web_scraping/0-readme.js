784f0bad27a07b394e8d343a8eeb05742a596dc7
root <nanamcroj@gmail.com>
2022-02-10 14:09:02 +0000

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-	if (err) {
-		console.error (err)
-		return
-	}
-	console.log(data);
+  if (err) {
+    console.error (err);
+    return;
+  }
+  console.log(data);
 });
