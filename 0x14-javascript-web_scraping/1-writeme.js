cfdac34199775f3d9ddfb9d33e61d48070a45e94
root <nanamcroj@gmail.com>
2022-02-10 15:13:14 +0000

    semistandd fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3],'utf8', err => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', err => {
   if (err) {
     console.error(err);
-    return;
   }
 });
