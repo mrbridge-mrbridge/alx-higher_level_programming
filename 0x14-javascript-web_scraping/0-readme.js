9ce4f95ab1240a725f295e5ae0773d4c480189ec
root <nanamcroj@gmail.com>
2022-02-10 14:14:43 +0000

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.error (err);
+    console.error(err);
     return;
   }
   console.log(data);
