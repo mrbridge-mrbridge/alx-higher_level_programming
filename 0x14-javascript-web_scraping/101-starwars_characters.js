f3e570e9a93ea40ddbe609a379845f8afae35240
root <nanamcroj@gmail.com>
2022-02-11 04:13:27 +0000

    lineup

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,8 +4,9 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 function swapi (error, response, body) {
   if (!error && response.statusCode === 200) {
     const characts = (JSON.parse(body).characters);
-    for (const item of characts) {
-      request(item, function (error, response, body) {
+    const size = characts.length;
+    for (let step = 0; step < size; step++) {
+      request(characts[step], function (error, response, body) {
         if (!error && response.statusCode === 200) {
           console.log(JSON.parse(body).name);
         }
