e82685685395d1ef840c61e1ead3b00ab0fcf5fd
root <nanamcroj@gmail.com>
2022-02-11 02:42:11 +0000

    for loop

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,9 +2,9 @@
 const request = require('request');
 function tasksCompleted (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const members = JSON.parse(response.body);
+    const members = JSON.parse(body);
     const compList = {};
-    for (const item in members) {
+    for (const item of members) {
       if (item.completed && compList[item.userId] === undefined) {
         compList[item.userId] = 1;
       } else if (item.completed) {
